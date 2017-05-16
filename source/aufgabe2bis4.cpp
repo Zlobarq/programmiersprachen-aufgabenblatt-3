8fd5b4034bcdb7c3b0b9b6adf3c21eb9c0dca0cc
Georgi Atanasov <noxo9240@billl3.in.uni-weimar.de>
2017-05-16 15:19:48 +0200

    update

---
+++ source/aufgabe2bis4.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <vector>
+#include <list>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+int main()
+{
+std::list<unsigned int> l= {0};
+return 0;
+};
